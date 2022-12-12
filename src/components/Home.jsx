12d16cc3739722e6cfbe7876e96ce50537d7d80a
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-12-12 17:25:36 +0300

    my portfolio

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -21,7 +21,7 @@ const Home = () => {
         
         <div>
           <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
-            View Work
+            DOWNLOAD CV
             <span className='group-hover:rotate-90 duration-300'>
               <HiArrowNarrowRight className='ml-3 ' />
             </span>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -58,7 +58,7 @@ const Navbar = () => {
    
          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
           <a  className='flex justify-between items-center w-full text-gray-300'
-          href='https://github.com/Tmureithi' target="_blank" rel="noreferrer noopener">
+          href='https://github.com/timothymureithi/' target="_blank" rel="noreferrer noopener">
             Github <FaGithub size={30} />
           </a>
           
